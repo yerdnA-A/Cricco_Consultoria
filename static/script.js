e435890c0eaab580fb694ffb55d2d504c0736f7c
yerdnA-A <andreycricco@gmail.com>
2025-11-04 14:36:46 -0300

    voltando para o fetch anterior

--- static/script.js
+++ static/script.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       try {
-        const resposta = await fetch("http://127.0.0.1:5000//api/cotacao", {
+        const resposta = await fetch("/api/cotacao", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(dados),
